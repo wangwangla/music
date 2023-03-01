8136afc7067a432ae5ae025156da71577fecddf6
28188 <2818815189@qq.com>
2023-03-02 00:36:21 +0800

    上传

--- app/src/main/java/kw/test/music/MainActivity.java
+++ app/src/main/java/kw/test/music/MainActivity.java
@@ -1,24 +1,32 @@
 package kw.test.music;
 
+import android.animation.Animator;
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
-import android.view.View;
-import android.view.Menu;
 
 import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.navigation.NavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.app.AppCompatActivity;
 
 import kw.test.music.databinding.ActivityMainBinding;
+import kw.test.music.databinding.ViewItemNavBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewTreeObserver;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration mAppBarConfiguration;
+    private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
 
     @Override
@@ -26,31 +34,60 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        setSupportActionBar(binding.appBarMain.toolbar);
-        DrawerLayout drawer = binding.drawerLayout;
-        NavigationView navigationView = binding.navView;
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
-                .setOpenableLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+        ViewItemNavBinding navView = binding.navView;
+
+
+        final ViewTreeObserver vto = navView.getRoot().getViewTreeObserver();
+        vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                float mHeaderWidth = navView.getRoot().getMeasuredWidth();
+                float mHeaderHeight = navView.getRoot().getMeasuredHeight();
+                //在API16之后removeOnGlobalLayoutListener代替了removeGlobalOnLayoutListener
+                if (android.os.Build.VERSION.SDK_INT <= android.os.Build.VERSION_CODES.JELLY_BEAN) {
+                    navView.getRoot().getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                } else {
+                    navView.getRoot().getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                }
+                navView.getRoot().setX(-mHeaderWidth);
+                float[] mm = {-mHeaderWidth,0};
+                Animator animator = ObjectAnimator.ofFloat(navView.getRoot(),"translationX",mm);
+                animator.start();
+//                Animation animation = new TranslateAnimation(80,100,0,100);
+//                animation.setDuration(4000);
+//                navView.getRoot().setAnimation(animation);
+            }
+        });
+
+
     }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.main, menu);
-//        return true;
-//    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
     }
 }
