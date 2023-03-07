58581de8435bec40804bce8c18b9762578e2b45a
kangwang <kangwang@doodlemobile.com>
2023-03-07 08:51:27 +0800

    修改布局

--- app/src/main/java/com/example/learnandroid/main/PlayerOrderFragment.java
+++ app/src/main/java/com/example/learnandroid/main/PlayerOrderFragment.java
@@ -4,11 +4,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.example.learnandroid.R;
+import com.example.learnandroid.adapter.SongAdapter;
+import com.example.learnandroid.bean.MusicBean;
+import com.example.learnandroid.constant.Constant;
+import com.example.learnandroid.constant.MusicManager;
+import com.example.learnandroid.data.ContentResolverFindMusic;
+
+import java.util.ArrayList;
 
 public class PlayerOrderFragment extends Fragment {
 
@@ -21,8 +30,39 @@ public class PlayerOrderFragment extends Fragment {
         return view;
     }
 
+    private int index;
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        ArrayList<MusicBean> musicBeans = new ArrayList<>();
+        ListView songList = view.findViewById(R.id.playlist);
+        SongAdapter adapter
+                = new SongAdapter(
+                getContext(),
+                R.layout.songlist_view_layout,
+                musicBeans
+        );
+        MusicManager.setSongList(musicBeans);
+        songList.setAdapter(adapter);
+//        TextView shuffePlayer = view.findViewById(R.id.shuffle_player);
+//        shuffePlayer.setText("顺序播放");
+//        View shuffle = view.findViewById(R.id.shuffle_btn);
+//        shuffle.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                index ++;
+//                if (index>=2){
+//                    index = 0;
+//                }
+//                if (index == 0){
+//                    shuffePlayer.setText("顺序播放");
+//                    Constant.playStyle = 0;
+//                }else if (index == 1){
+//                    shuffePlayer.setText("随机播放");
+//                    Constant.playStyle = 1;
+//                }
+//            }
+//        });
     }
 
     @Override
