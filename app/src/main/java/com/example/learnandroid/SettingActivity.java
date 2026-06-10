c2999ae319bc7c6a4b387a9900349be2bf399b58
tony <kangwang@doodlemobile.com>
2026-06-10 19:15:25 +0800

    保存

---
+++ app/src/main/java/com/example/learnandroid/SettingActivity.java
@@ -0,0 +1,29 @@
+package com.example.learnandroid;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import kw.learn.mylibrary.activity.BaseActivity;
+
+public class SettingActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    @Override
+    protected int getResourceId() {
+        return R.layout.activity_setting;
+    }
+}
